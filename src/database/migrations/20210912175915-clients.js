4a24725c205ecece92f288329f3ada656739ac10
Lucas Almeida <lucasaclima03@gmail.com>
2021-09-12 15:06:05 -0300

    criação da migration de produtos

---
+++ src/database/migrations/20210912175915-clients.js
@@ -0,0 +1,37 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('clients', {
+    id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    cpf: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    address: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    phone: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    created_at: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+    updated_at: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+  }),
+
+  down: (queryInterface) => queryInterface.droptable('clients'),
+
+};
